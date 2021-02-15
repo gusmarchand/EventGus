372d520e343249bbfd74b35d94c5ce855c9083c9
Augustin Marchand <gus@macbook-pro-de-augustin.home>
2021-02-15 18:50:50 +0100

    2nd commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
 
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
   has_many :events
   has_many :attendances 
   has_many :events, through: :attendances
