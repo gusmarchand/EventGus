1910f0998ac57a87b483f4d15306ff3ce64998b8
Augustin Marchand <gus@macbook-pro-de-augustin.home>
2021-02-15 19:58:37 +0100

    test

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :start_date, presence: true
-  validates :duration, presence:true, numericality: { greater_than : 0}, #ajout multiple de 5
+  #validates :duration, presence:true, numericality: { greater_than : 0}, #ajout multiple de 5
   validates :title, presence:true, length: { in: 5..140 }
   validates :description, presence:true , length: { in: 20..1000 }
   validates :price, presence:true, numericality: { in: 1..1000 } #???
